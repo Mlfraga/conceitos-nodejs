a1279d3b8ca3be21a7a8a83467ec014f543e9d3c
Matheus Fraga <matheuslf44@gmail.com>
2020-06-29 17:51:24 -0300

    finishing the challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
 
-// const { uuid } = require("uuidv4");
+function authenticateUuid(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response
+      .status(400)
+      .json({ error: "This id is not valid." })
+  }
+
+  next();
+}
 
 const app = express();
 
@@ -11,23 +22,75 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response
+    .status(200)
+    .json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs: techs,
+    likes: 0
+  }
+
+  if (!title || !url || !techs) {
+    return response
+      .status(400)
+      .json({ error: "This informations is not valid." })
+  }
+
+  repositories.push(repository);
+
+  return response
+    .status(200)
+    .json(repository);
+
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", authenticateUuid, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryByIndex = repositories.findIndex(repository => repository.id === id)
+
+  repositories[repositoryByIndex].title = title;
+  repositories[repositoryByIndex].url = url;
+  repositories[repositoryByIndex].techs = techs;
+
+  return response
+    .status(200)
+    .json(repositories[repositoryByIndex])
+
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", authenticateUuid, (request, response) => {
+  const { id } = request.params;
+
+  const repositoryByIndex = repositories.findIndex(repository => repository.id === id)
+
+  repositories.splice(repositoryByIndex, 1)
+
+  return response
+    .status(204)
+    .json({ message: "Repository was succesfully deleted." })
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", authenticateUuid, (request, response) => {
+  const { id } = request.params;
+
+  const repositoryByIndex = repositories.findIndex(repository => repository.id === id)
+
+  repositories[repositoryByIndex].likes = repositories[repositoryByIndex].likes + 1;
+
+  return response
+    .status(200)
+    .json(repositories[repositoryByIndex])
+
 });
 
 module.exports = app;
